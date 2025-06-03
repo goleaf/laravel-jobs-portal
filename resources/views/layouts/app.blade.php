0e20ff82145ed4b3068765b6d33cbd5cd934720d
TodoApp Admin <admin@todoapp.prus.dev>
2025-06-04 01:30:23 +0300

    Fix responsive-nav-link component error by removing unused component references from layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -108,14 +108,6 @@
 
     <!-- Notification Component -->
     <x-notification />
-
-    <x-responsive-nav-link :href="route('components.icon-documentation')" :active="request()->routeIs('components.icon-documentation')">
-        {{ __('Component Documentation') }}
-    </x-responsive-nav-link>
-
-    <x-responsive-nav-link :href="route('icons.documentation')" :active="request()->routeIs('icons.documentation')">
-        {{ __('Icon System') }}
-    </x-responsive-nav-link>
 </body>
 <!--end::Body-->
 
